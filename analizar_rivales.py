9e5a035fe937965525a868746b8f9a964c97734e
Andres Cucchiarini <acucchiarini@fi.uba.ar>
2026-03-28 15:28:30 -0300

    Corrijo modulo analizar_rivales

--- analizar_rivales.py
+++ analizar_rivales.py
@@ -1,17 +1,13 @@
 def tiempo_minimo(rivales):
-    rivales.sort(key=lambda rival: rival[1], reverse=True)
+    rivales.sort(key=lambda rival: int(rival[1]), reverse=True)
 
     tiempo_scaloni = 0
-    tiempo_total = 0
 
     for tiempos in rivales:
         Si = int(tiempos[0])
         Ai = int(tiempos[1])
 
         tiempo_scaloni += Si
-        fin = tiempo_scaloni + Ai
+        fin_analisis = tiempo_scaloni + Ai
 
-        if fin > tiempo_total:
-            tiempo_total = fin
-
-    return tiempo_total
+    return fin_analisis
